aaa96557f4fc165669b50f57b7f4c60b701952a0
ingswgruppo13 <60515593+ingswgruppo13@users.noreply.github.com>
2020-05-28 14:57:30 +0200

    Progetto2

---
+++ stdinutils.c
@@ -0,0 +1,34 @@
+//@author Abbate Carmine - N86002234
+#include <stdio.h>
+#include <stdlib.h>
+#include "stdinutils.h"
+//Funzioni di input
+
+char* read()
+{
+    unsigned size = BUFSIZE;
+    char *buf = NULL;
+    int c, i;
+
+    size = i = 0;
+    do {
+        size += BUFSIZE;
+        buf = realloc(buf, size);
+        while (i < size && (c = getchar()) != '\n')
+            buf[i++] = c;
+    } while (c != '\n');
+    if (!i) {
+        free(buf);
+        return NULL;
+    }
+    buf[i] = '\0';
+    return buf;
+}
+
+int readint()
+{
+    char *char_input = read();
+    int input = atoi(char_input);
+    free(char_input);
+    return input;
+}
