e57b54afac1592e45ee4f06cbbc09f70fe73635c
Fabio De Martino <65670780+Fabiodm97@users.noreply.github.com>
2020-05-29 13:53:47 +0200

    Update graph.c

--- Progetto2/graph.c
+++ Progetto2/graph.c
@@ -1,28 +1,79 @@
-#ifndef GRAPH_H_INCLUDED
-#define GRAPH_H_INCLUDED
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include"graph.h"
 
-typedef struct arco{
-  char *localita;
-  struct arco *next;
-  int costo;
-}arco;
-
-typedef struct grafo{
-  int n_vertici;
-  arco **adiacenti;
-}grafo;
+int empty_graph(struct grafo* G){
+	return G==NULL;
+}
 
 
-int empty_graph(struct grafo*);
+int creaGrafo(int vertici, grafo **g, int pesato){
 
-int creaGrafo(int, grafo **, int);
+  grafo *nuovo;
+  int ret = 0;//inizializzata a 0 per gestire implicitamente i casi in cui non fosse possibile allocare memoria
+  int i;
 
-void stampaGrafo(grafo *);
+  if(vertici > 0){//alloca memoria solo se vertici e' maggiore di 0
+    nuovo = (grafo*)malloc(sizeof(grafo));//alloca memoria per un nuovo grafo
 
-arco *nuovoArco(int, int );
+    if(nuovo != NULL){//se ha allocato memoria per il grafo
+      nuovo->n_vertici = vertici;
+      nuovo->adiacenti = (arco**)malloc(sizeof(arco*) * vertici);//alloca memoria per il vettore di liste
+
+      if(nuovo->adiacenti != NULL){//se ha allocato il vettore di liste
+        for(i = 0; i < vertici; i++){//pone tutte le liste a NULL
+          nuovo->adiacenti[i] = NULL;
+        }
+
+        *g = nuovo;//pone il valore del puntatore parametro alla posizione del grafo allocato
+        ret = 1;
+
+      }else{//se la malloc del vettore e' fallita
+        printf("ERRORE in nuovoGrafo: impossibile allocare memoria per vettore di liste di adiacenza\n");
+        free(nuovo);//dealloca la memoria del grafo
+      }
+    }else{//caso memoria per grafo non allocata gestito implicitamente
+      printf("ERRORE in nuovoGrafo: impossibile allocare memoria per grafo\n");
+    }
+  }else{//caso vertici <= 0 gestito implicitamente
+    printf("ERRORE in nuovoGrafo: impossibile creare grafo di 0 vertici\n");
+  }
+  return ret;//ritorna 1 o 0 a seconda che la creazione sia andata a buon fine o meno
+}
 
 
-#endif // GRAPH_H_INCLUDED
+void stampaGrafo(struct grafo* G){
+	int ca=0, i; arco* app;
+	if(!empty_graph(G)){
+		printf("IL GRAFO HA VERTICI %d", G->n_vertici);
+		printf("\n");
+		for(i=0;i<G->n_vertici;i++){
+			printf("NODI ADIACENTI AL VERTICE %d", i);
+			printf("\n");
+			app=G->adiacenti[i];
+			while(app!=NULL){
+				printf("%s",app->localita);
+				printf("\n");
+				ca++;
+				app=app->next;
+			}
+		}
+	}
+	printf("IL GRAFO HA ARCHI %d", ca/2);
+	printf("\n");
+}
+
+arco *nuovoArco(int destinazione, int costo){
+
+  arco *nuovo = NULL;
+
+  nuovo = (arco*)malloc(sizeof(arco));
+  if(nuovo != NULL){
+    nuovo->localita = destinazione;
+    nuovo->next = NULL;
+    nuovo->costo = costo;
+  }else{
+    printf("ERRORE: memoria insufficiente\n");
+  }
+
+  return nuovo;
+}
+
