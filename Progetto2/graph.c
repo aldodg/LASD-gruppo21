a313b58ea4d2762170994e1ddf49120d33b89238
Fabio De Martino <65670780+Fabiodm97@users.noreply.github.com>
2020-05-29 13:53:03 +0200

    Update graph.c

--- Progetto2/graph.c
+++ Progetto2/graph.c
@@ -1,3 +1,28 @@
-#include"graph.h"
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct arco{
+  char *localita;
+  struct arco *next;
+  int costo;
+}arco;
+
+typedef struct grafo{
+  int n_vertici;
+  arco **adiacenti;
+}grafo;
 
 
+int empty_graph(struct grafo*);
+
+int creaGrafo(int, grafo **, int);
+
+void stampaGrafo(grafo *);
+
+arco *nuovoArco(int, int );
+
+
+#endif // GRAPH_H_INCLUDED
