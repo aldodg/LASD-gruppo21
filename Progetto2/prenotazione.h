51df72eff6097bb43d299bd9fbc2e27c0e8b2851
Fabio De Martino <65670780+Fabiodm97@users.noreply.github.com>
2020-06-06 18:28:44 +0200

    Update prenotazione.h

--- Progetto2/prenotazione.h
+++ Progetto2/prenotazione.h
@@ -4,24 +4,26 @@
 
 struct voliPrenotati{
 		    char *partenza;
-		    char *arrivo;
+		    char *destinazione;
 		    int costo;
+		    int durata;
 		    struct voliPrenotati *next;
 		};
 	typedef struct voliPrenotati *Prenotati;
 
 
-/*INIZIALIZZA UN NUOVO NODO*/
-Prenotati initNode(char *,char *, int);
+/* INIZIALIZZA UN NUOVO NODO*/
+Prenotati initNode(char *,char *, int, int);
 
 /* FUNZIONE CHE AGGIUNGE UN NODO ALLA FINE DELLA LISTA*/
-Prenotati insertTail(Prenotati P, char * , char *, int );
+Prenotati insertTail(Prenotati P, char * , char *, int, int);
 
 /* FUNZIONE CHE AGGIUNGE UN NODO IN TESTA ALLA LISTA*/
-Prenotati insertHead(Prenotati P, char * ,char *, int );
+Prenotati insertHead(Prenotati P, char * ,char *, int, int);
 
 /* STAMPA LA LISTA DEI VOLI PRENOTATI*/
 void printVoliPrenotati(Prenotati P);
 
 /* DEALLOCA LA LISTA DEI VOLI PRENOTATI */
 void freeVoliPrenotati(Prenotati P);
+
