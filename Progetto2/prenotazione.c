b70b6cde86990fc8bbd301ce246fd17033523e09
Fabio De Martino <65670780+Fabiodm97@users.noreply.github.com>
2020-06-06 18:29:22 +0200

    Update prenotazione.c

--- Progetto2/prenotazione.c
+++ Progetto2/prenotazione.c
@@ -1,42 +1,44 @@
 #include "prenotazione.h"
 
-Prenotati initNode(char *partenza, char *arrivo, int costo)
+Prenotati initNode(char *partenza, char *destinazione, int costo, int durata)
 {
-    Prenotati P = (Prenotati) malloc(sizeof(struct voliPrenotati));
+    Prenotati P = (Prenotati)malloc(sizeof(struct voliPrenotati));
     P->partenza = partenza;
-    P->arrivo = arrivo;
+    P->destinazione = destinazione;
     P->costo = costo;
+    P->durata = durata;
     P->next = NULL;
     return P;
 }
 
 
-Prenotati insertTail(Prenotati P, char *partenza, char *arrivo, int costo)
+Prenotati insertTail(Prenotati P, char *partenza, char *destinazione, int costo, int durata)
 {
     if (P != NULL)
     {
-        P->next = insertTail(P->next, partenza, arrivo, costo);
+        P->next = insertTail(P->next, partenza, destinazione, costo, durata);
     }
     else
     {
-        P = initNode(partenza,arrivo,costo);
+        P = initNode(partenza,destinazione,costo,durata);
     }
     return P;
 }
 
 
-Prenotati insertHead(Prenotati P, char *partenza,char *arrivo, int costo)
+Prenotati insertHead(Prenotati P, char *partenza,char *destinazione, int costo, int durata)
 {
     if (P != NULL)
     {
         Prenotati X = (Prenotati )malloc(sizeof(struct voliPrenotati));
         X->partenza = partenza;
-        X->arrivo = arrivo;
+        X->destinazione = destinazione;
         X->costo = costo;
+        X->durata=durata;
         X->next = P;
         return X;
     }
-    return initNode(partenza, arrivo, costo);
+    return initNode(partenza, destinazione, costo, durata);
 }
 
 
@@ -44,8 +46,9 @@ void printVoliPrenotati(Prenotati P)
 {
     if (P != NULL)
     {
-        printf(" %s %s ", P->partenza, P->arrivo);
+        printf(" %s %s ", P->partenza, P->destinazione);
         printf(" %d ", P->costo);
+        printf(" %d ", P->durata);
         printVoliPrenotati(P->next);
     }
 }
